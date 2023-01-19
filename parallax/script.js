348aaac44dc403ccd6eec4dedf1f8fc86dd087f7
Adryann Millos Santos de Freitas <adryann.freitas@ubistart.com>
2023-01-19 10:18:46 -0300

    feat: remove gameFrame dynamic

--- parallax/script.js
+++ parallax/script.js
@@ -4,7 +4,6 @@ const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
 
 let gamespeed = 5;
-let gameFrame = 0;
 
 const backGroundLayer1 = new Image();
 backGroundLayer1.src = "layer-1.png";
@@ -21,57 +20,60 @@ backGroundLayer4.src = "layer-4.png";
 const backGroundLayer5 = new Image();
 backGroundLayer5.src = "layer-5.png";
 
-const slider = document.getElementById("slider");
-slider.value = gamespeed;
-const showGameSpeed = document.getElementById("showGameSpeed");
-showGameSpeed.innerHTML = gamespeed;
-slider.addEventListener("change", function (e) {
-  gamespeed = e.target.value;
-  showGameSpeed.innerHTML = e.target.value;
-});
-
-class Layer {
-  constructor(image, speedModifier) {
-    this.x = 0;
-    this.y = 0;
-    this.width = 2400;
-    this.height = 700;
-    this.x2 = this.width;
-    this.image = image;
-    this.speedModifier = speedModifier;
-    this.speed = gamespeed * this.speedModifier;
-  }
-  update() {
-    this.speed = gamespeed * this.speedModifier;
-
-    this.x = (gameFrame * this.speed) % this.width;
-  }
-  draw() {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(
-      this.image,
-      this.x + this.width,
-      this.y,
-      this.width,
-      this.height
-    );
-  }
-}
-
-const layer1 = new Layer(backGroundLayer1, 0.2);
-const layer2 = new Layer(backGroundLayer2, 0.4);
-const layer3 = new Layer(backGroundLayer3, 0.6);
-const layer4 = new Layer(backGroundLayer4, 0.8);
-const layer5 = new Layer(backGroundLayer5, 1);
-
-const gameObjects = [layer1, layer2, layer3, layer4, layer5];
-function animate() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  gameObjects.forEach((item) => {
-    item.update();
-    item.draw();
+window.addEventListener("load", function () {
+  const slider = document.getElementById("slider");
+  slider.value = gamespeed;
+  const showGameSpeed = document.getElementById("showGameSpeed");
+  showGameSpeed.innerHTML = gamespeed;
+  slider.addEventListener("change", function (e) {
+    gamespeed = e.target.value;
+    showGameSpeed.innerHTML = e.target.value;
   });
-  gameFrame--;
-  requestAnimationFrame(animate);
-}
-animate();
+
+  class Layer {
+    constructor(image, speedModifier) {
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 700;
+      this.x2 = this.width;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gamespeed * this.speedModifier;
+    }
+    update() {
+      this.speed = gamespeed * this.speedModifier;
+      if (this.x <= -this.width) {
+        this.x = 0;
+      }
+      this.x = this.x -this.speed
+    }
+    draw() {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(
+        this.image,
+        this.x + this.width,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+  }
+
+  const layer1 = new Layer(backGroundLayer1, 0.2);
+  const layer2 = new Layer(backGroundLayer2, 0.4);
+  const layer3 = new Layer(backGroundLayer3, 0.6);
+  const layer4 = new Layer(backGroundLayer4, 0.8);
+  const layer5 = new Layer(backGroundLayer5, 1);
+
+  const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+  function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    gameObjects.forEach((item) => {
+      item.update();
+      item.draw();
+    });
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
